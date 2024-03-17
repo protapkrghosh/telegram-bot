72f08bf9c3df34ab0351fd714a4255ea7b483f5e
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-17 23:59:58 +0600

    home section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <>
-      <h1 className='text-white'>Now paste the home content here</h1>
+      <h1 className='text-white'>Now paste the home content</h1>
     </>
   );
 };
