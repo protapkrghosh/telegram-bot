021819e34ff6021361dff2baae1c3305e50459b5
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-18 09:51:20 +0600

    featus

--- src/components/sections/Features/Features.jsx
+++ src/components/sections/Features/Features.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Container from '../Container/Container';
 import featuresIcon1 from '@/assets/features/Group64.png';
+import featuresIcon2 from '@/assets/features/Group65.png';
+import featuresIcon3 from '@/assets/features/Group66.png';
 
 const Features = () => {
   return (
@@ -16,15 +18,15 @@ const Features = () => {
               <p className='mb-[16px] text-[#EAE8E1] font-manrope text-[26px] font-bold leading-[36.4px]'>Block analysis</p>
               <p className='mb-[30px] text-[#A5A39B] font-inter leading-[25.6px]'>Was the launch bundled? Who was first? How much did they bribe?</p>
             </div>
-            <div className='border'>
-              <img className='mb-[30px]' src={featuresIcon1} alt="" />
-              <p className='mb-[16px] text-[#EAE8E1] font-manrope text-[26px] font-bold leading-[36.4px]'>Block analysis</p>
-              <p className='mb-[30px] text-[#A5A39B] font-inter leading-[25.6px]'>Was the launch bundled? Who was first? How much did they bribe?</p>
+            <div className='px-[30px] pt-[30px] border-2 border-[#201B15] rounded-[30px]'>
+              <img className='mb-[30px]' src={featuresIcon2} alt="" />
+              <p className='mb-[16px] text-[#EAE8E1] font-manrope text-[26px] font-bold leading-[36.4px]'>Tax inspector</p>
+              <p className='mb-[30px] text-[#A5A39B] font-inter leading-[25.6px]'>Did the launch have tax? How much money did the tax wallet receive?</p>
             </div>
-            <div className='border'>
-              <img className='mb-[30px]' src={featuresIcon1} alt="" />
-              <p className='mb-[16px] text-[#EAE8E1] font-manrope text-[26px] font-bold leading-[36.4px]'>Block analysis</p>
-              <p className='mb-[30px] text-[#A5A39B] font-inter leading-[25.6px]'>Was the launch bundled? Who was first? How much did they bribe?</p>
+            <div className='px-[30px] pt-[30px] border-2 border-[#201B15] rounded-[30px]'>
+              <img className='mb-[30px]' src={featuresIcon3} alt="" />
+              <p className='mb-[16px] text-[#EAE8E1] font-manrope text-[26px] font-bold leading-[36.4px]'>Insider tracker</p>
+              <p className='mb-[30px] text-[#A5A39B] font-inter leading-[25.6px]'>Enter a list of addresses to cross reference tokens they all bought.</p>
             </div>
           </div>
         </div>
