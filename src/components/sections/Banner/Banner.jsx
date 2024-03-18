218d0d66ef627d11eddeb6c03b4ee16827e0cd7c
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-19 01:28:59 +0600

    banner section

--- src/components/sections/Banner/Banner.jsx
+++ src/components/sections/Banner/Banner.jsx
@@ -32,10 +32,10 @@ const Banner = () => {
           <Container>
             <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-5 mt-10 2xl:mt-32">
               <div className="bgDropShadow w-fit p-8">
-                <h1 className="text-white text-[50px] md:text-[57px] lg:text-[75px] 2xl:text-[80px] font-manrope font-semibold">Early Bird</h1>
-                <p className="text-[#BBB9AF] text-[21px] lg:text-[25px] 2xl:text-[32px] font-inter mb-5">Get the information you <br /> need, fast.</p>
+                <h1 className="text-white text-[50px] md:text-[57px] lg:text-[75px] 2xl:text-[80px] font-manrope font-semibold" data-aos="fade-up" data-aos-duration="1000">Early Bird</h1>
+                <p className="text-[#BBB9AF] text-[21px] lg:text-[25px] 2xl:text-[32px] font-inter mb-5" data-aos="fade-up" data-aos-duration="2000">Get the information you <br /> need, fast.</p>
 
-                <Button className="text-[#151414] rounded-[8px] group cursor-pointer md:px-5">
+                <Button className="text-[#151414] rounded-[8px] group cursor-pointer md:px-5" data-aos="fade-up" data-aos-duration="3000">
                   <span className="flex justify-center items-center">
                     <p className='text-[16px] 2xl:text-[18px] font-inter'>Launch App</p>
                     <MdArrowOutward className='text-[18px] ml-2 group-hover:-mt-2 duration-300' />
