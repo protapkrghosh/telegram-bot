f0b60c91f49ab36cf0dbbe4421b4e2d6d566f27d
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-18 23:55:02 +0600

    tokenmiss

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -7,7 +7,7 @@ const Tokenomics = () => {
       <Container>
         <div>
           <div>
-            {/* token title  */}
+            {/*  token title   */}
             <div>
               <h2 className='text-[#EAE8E1] text-center font-manrope text-[47px] font-bold mb-[13px]'>Tokenomics</h2>
               <p className='text-[#A5A39B] text-center font-inter text-[18px] w-[50%] mx-auto'>Tokenomics involves designing incentives and mechanisms to govern the distribution and use of digital tokens within a system.</p>
