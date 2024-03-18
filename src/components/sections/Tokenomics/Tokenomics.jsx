9a4e976a0670f4d378049fff564e1c52fd66b776
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-18 10:25:45 +0600

    token

---
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from '../Container/Container';
+
+const Tokenomics = () => {
+  return (
+    <>
+      <Container>
+        <div>
+          <div>
+
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Tokenomics;
