4370e4034e0b7cfc5159371b5cbba365d2f5c0a7
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-18 09:27:03 +0600

    features

--- src/components/sections/Features/Features.jsx
+++ src/components/sections/Features/Features.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Container from '../Container/Container';
+import featuresIcon1 from '@/assets/features/Group64.png';
 
 const Features = () => {
   return (
     <>
       <Container>
         <div>
+          {/* title part  */}
           <h2 className='mb-[60px] text-[#EAE8E1] font-manrope text-[47px] font-bold leading-[54.05px]'>Key Features </h2>
+          {/* card section  */}
+          <div>
+            <div>
+              <img className='mb-[30px]' src={featuresIcon1} alt="" />
+              <p className='mb-[16px] text-[#EAE8E1] font-manrope text-[26px] font-bold leading-[36.4px]'>Block analysis</p>
+              <p className='mb-[30px] text-[#A5A39B] font-inter leading-[25.6px]'>Was the launch bundled? Who was first? How much did they bribe?</p>
+            </div>
+          </div>
         </div>
       </Container>
     </>
