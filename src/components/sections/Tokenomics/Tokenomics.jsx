19a0f1e05601ffc8ca0a70bf4d84cb16ef032301
Protap Ghosh <protapg518@gmail.com>
2024-03-19 01:43:51 +0600

    d

--- src/components/sections/Tokenomics/Tokenomics.jsx
+++ src/components/sections/Tokenomics/Tokenomics.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import circleImg from '@/assets/tokenomics/Group 29.png';
 import Container from '../Container/Container';
 
 const Tokenomics = () => {
   return (
-    <div id='tokenomics'>
+    <div className='my-20 xl:my-36' id='tokenomics'>
       <Container>
-        <div className='mt-[160px]'>
+        <div className='bg-gradient-to-r from-[#201B15] to-[#1B1915] border border-[#28221E] rounded-[20px] px-20 xl:px-24 py-14 xl:py-16'>
+          {/*  token title   */}
           <div>
-            {/*  token title   */}
+            <h2 className='text-[#EAE8E1] text-center font-manrope text-[47px] font-bold mb-[13px]'>Tokenomics</h2>
+            <p className='text-[#A5A39B] text-center font-inter text-[17px] 2xl:text-[18px] w-[72%] mx-auto'>Tokenomics involves designing incentives and mechanisms to govern the distribution and use of digital tokens within a system.</p>
+          </div>
+
+          {/* Card content area */}
+          <div className='mt-[60px]'>
+            {/* Circle */}
             <div>
-              <h2 className='text-[#EAE8E1] text-center font-manrope text-[47px] font-bold mb-[13px]'>Tokenomics</h2>
-              <p className='text-[#A5A39B] text-center font-inter text-[18px] w-[50%] mx-auto'>Tokenomics involves designing incentives and mechanisms to govern the distribution and use of digital tokens within a system.</p>
+              <img src={circleImg} alt="Image" className='w-[390px]' />
             </div>
-            {/* circle  */}
-            <div className='mt-[60px]'>
-              {/* left section */}
-              <div>
 
-              </div>
-              {/* right section  */}
-              <div>
+            {/* Counter  */}
+            <div>
 
-              </div>
             </div>
           </div>
         </div>
