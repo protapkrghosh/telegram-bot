e049b2e74891d5e427e91ddbfff80a67f82f4070
sagar-ghosh1 <sagorghosh520@gmail.com>
2024-03-20 10:47:51 +0600

    avout

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -20,26 +20,23 @@ const About = () => {
           {/*  right area  */}
           <div className='w-[100%] md:w-[75%]'>
             <div className='w-[70%] md:w-[85%]'>
-              <h2 className='text-[#FFF] font-manrope text-[47px] font-bold' data-aos="fade-down"
-                data-aos-easing="linear"
-                data-aos-duration="1000">About Early Bird</h2>
+              <h2 className='text-[#FFF] font-manrope text-[47px] font-bold' data-aos="fade-up"
+                data-aos-duration="800">About Early Bird</h2>
               <p className='text-[#A5A39B] font-inter text-[18px] leading-[28.8px] mt-[20px]' data-aos="fade-up"
-                data-aos-duration="2000">Early Bird allows you to quickly get crucial info on a token's launch. Simply enter the contract address for the token and Early Bird will relay all the launch information you require.</p>
+                data-aos-duration="1300">Early Bird allows you to quickly get crucial info on a token's launch. Simply enter the contract address for the token and Early Bird will relay all the launch information you require.</p>
               <img className='mt-[24px] mb-[24px]' src={aboutLineImg} alt="" />
-              <div className='flex items-center space-x-[10px] mb-[16px]' data-aos="fade-down"
-                data-aos-easing="linear"
-                data-aos-duration="1000">
+              <div className='flex items-center space-x-[10px] mb-[16px]' data-aos="fade-up"
+                data-aos-duration="1800">
                 <div className='text-[#CDBB13]'><FiCheckCircle /></div>
                 <div><p className='text-[#EAE8E1] font-manrope text-[20px] font-medium leading-[28px]'>Swift access to crucial information.</p></div>
               </div>
               <div className='flex items-center space-x-[10px] mb-[16px]' data-aos="fade-up"
-                data-aos-duration="2000">
+                data-aos-duration="2300">
                 <div className='text-[#CDBB13]'><FiCheckCircle /></div>
                 <div><p className='text-[#EAE8E1] font-manrope text-[20px] font-medium leading-[28px]'>Faster than going through Etherscan.</p></div>
               </div>
-              <div className='flex items-center space-x-[10px]' data-aos="fade-down"
-                data-aos-easing="linear"
-                data-aos-duration="1000">
+              <div className='flex items-center space-x-[10px]' data-aos="fade-up"
+                data-aos-duration="2700">
                 <div className='text-[#CDBB13]'><FiCheckCircle /></div>
                 <div><p className='text-[#EAE8E1] font-manrope text-[20px] font-medium leading-[28px]' >Clean layout for ease of access.</p></div>
               </div>
